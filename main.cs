89b0f5b5e82d49822657532e5924dfdba9f42479
Knievil <105438364+Knievil@users.noreply.github.com>
2023-09-25 08:56:13 -0500

    Create main.cs
    added main file

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ShowBizRadio // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running");
+        }
+    }
+}
